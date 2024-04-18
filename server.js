4842e406986a0c20bc6875bf248494de14f67b29
JwanMardini <jwan.99.mardini@gmail.com>
2024-04-18 11:00:44 +0200

    initialized the project with lab requirments

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('dist'));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on: http://localhost:${port}`);
+});
